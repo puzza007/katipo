ba65447d87306b79be4dde499ed85c8093acf1be
Paul Oliver <puzza007@gmail.com>
2025-12-28 20:10:23 +1300

    fix(test): re-enable self-signed certificate tests
    The verify_host_verify_peer_error and cacert_self_signed tests were
    disabled due to transient CI failures. The external service
    self-signed.badssl.com is now working reliably.

--- test/katipo_SUITE.erl
+++ test/katipo_SUITE.erl
@@ -183,10 +183,8 @@ groups() ->
        max_concurrent_streams]},
      {https, [parallel],
       [verify_host_verify_peer_ok,
-       %% TODO :Fix this test. See https://github.com/puzza007/katipo/runs/5281801454?check_suite_focus=true
-       %% verify_host_verify_peer_error,
-       %% TODO: Fix this test. See https://github.com/puzza007/katipo/runs/5281750037?check_suite_focus=true
-       %% cacert_self_signed,
+       verify_host_verify_peer_error,
+       cacert_self_signed,
        capath,
        capath_string,
        cacert_string,
